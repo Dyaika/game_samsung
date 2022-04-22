877d398b391905fd2895b00d39997087307c2099
Dyaika <91870391+Dyaika@users.noreply.github.com>
2022-04-22 11:35:49 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,55 +1,71 @@
 package com.company;
-
-import java.util.Scanner;
 public class Main {
+    static void printMain(Filter[][] matrix, int height){
+        int width = 4;
+        for (int y = height - 1; y >= 0; y--){
+            for (int x = 0; x < width; x++){
+                System.out.print(matrix[x][y].getColor() + "" + matrix[x][y].getShape() + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        int height = 12;//можно менять
-        Filter[][] arr = new Filter[height][4];
-        Filter[] lastRow = new Filter[4];
-        arr[0][0] = new Filter(0, 0);
-        arr[0][1] = new Filter(1, 1);
-        arr[0][2] = new Filter(2, 2);
-        arr[0][3] = new Filter(3, 3);//создаем лунки
-        lastRow[0] = arr[0][0];
-        lastRow[1] = arr[0][1];
-        lastRow[2] = arr[0][2];
-        lastRow[3] = arr[0][3];//чтобы мы могли "видеть" что в дырах
+        int height = 7;//можно менять
+        Filter[][] arr = new Filter[4][height];
+        for(int j = 0; j < height; j++) {
+            for (int i = 0; i < 4; i++) {
+                arr[i][j] = new Filter();
+            }
+        }
+        arr[0][0].makeSpace();
+
+        Filter.generateMatrix(arr, height);
+        printMain(arr, height);
+        /*
+        int height = 30;//можно менять
+        FilterOld[][] arr = new FilterOld[height][4];
+        FilterOld[] lastRow = new FilterOld[4];
+        for (int i = 0; i < 4; i++){
+            arr[0][i] = new FilterOld(i, i);
+            lastRow[i] = arr[0][i];//чтобы мы могли "видеть" что в дырах
+        }
+        int state;
+        int diff;
         for (int i = 1; i < height; i++){
             int right = (int)Math.round(Math.random());//0 дырка слева 1 справа
             if (right == 1){
                 for (int j = 0; j < 3; j++){
-                    int diff = (int)Math.round(Math.random());//0 совпадет состояние, 1 совпадет цвет
-                    int state = (int)Math.round(Math.random() * 3);
-                    if (diff == 0){
-                        arr[i][j] = new Filter(lastRow[j].getSub(), state);
-                        lastRow[j] = arr[i][j];
-                    } else {
-                        arr[i][j] = new Filter(state, lastRow[j].getCol());
-                        lastRow[j] = arr[i][j];
-                    }
+                    diff = (int)Math.round(Math.random());//0 совпадет состояние, 1 совпадет цвет
+                    state = (int)Math.round(Math.random() * 3);
+                    if (diff == 0){arr[i][j] = new FilterOld(lastRow[j].getSub(), state);
+                    } else {arr[i][j] = new FilterOld(state, lastRow[j].getCol());}
+                    lastRow[j] = arr[i][j];
                 }
-                arr[i][3] = new Filter(4, 4);//дыра
+                arr[i][3] = new FilterOld(4, 4);//дыра
             } else {
                 for (int j = 1; j < 4; j++){
-                    int diff = (int)Math.round(Math.random());//0 совпадет состояние, 1 совпадет цвет
-                    int state = (int)Math.round(Math.random() * 3);
+                    diff = (int)Math.round(Math.random());//0 совпадет состояние, 1 совпадет цвет
+                    state = (int)Math.round(Math.random() * 3);
                     if (diff == 0){
-                        arr[i][j] = new Filter(lastRow[j].getSub(), state);
-                        lastRow[j] = arr[i][j];
+                        if (lastRow[j].getCol() == state){state = (state + 1) % 4;}
+                        arr[i][j] = new FilterOld(lastRow[j].getSub(), state);
                     } else {
-                        arr[i][j] = new Filter(state, lastRow[j].getCol());
-                        lastRow[j] = arr[i][j];
+                        if (lastRow[j].getSub() == state){state = (state + 1) % 4;}
+                        arr[i][j] = new FilterOld(state, lastRow[j].getCol());
                     }
+                    lastRow[j] = arr[i][j];
                 }
-                arr[i][0] = new Filter(4, 4);//дыра
+                arr[i][0] = new FilterOld(4, 4);//дыра
             }
 
         }
         for (int i = height - 1; i > -1; i--){
             for (int j = 0; j < 4; j++){
-                System.out.print(arr[i][j] + "         ");
+                System.out.print(arr[i][j] + "\t");
             }
             System.out.println("");
-        }
+        }*/
+
     }
+
 }
